ae1ca2f962276097dbd23852f3dc719260fd3215
Arne Müller <arne.mueller@fu-berlin.de>
2014-03-30 20:42:12 +0200

    added some test cases and wrote some preliminary interface classes for modules (I though I need them for the test cases, but then realized I actually don't need them. But eventually we will need them).

---
+++ SP/src/project/Modules.java
@@ -0,0 +1,16 @@
+package project;
+
+import java.util.Set;
+
+/**
+ * Interface to represent the computed modules.
+ * @author arne
+ *
+ */
+public interface Modules {
+	/**
+	 * Returns a list with the computed modules.
+	 * @return a list of modules.
+	 */
+	public Set<Module> getModules();
+}
---
+++ SP/src/test/TestPrintModules.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPrintModules {
+
+	@Test
+	public void testPrintModule() {
+		fail("Not yet implemented");
+	}
+
+	
+	@Test
+	public void testPrintModules() {
+		fail("Not yet implemented");
+	}
+}
